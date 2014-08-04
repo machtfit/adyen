50b441be47b52f94659d00007835d9e4723fbb70
Markus Bertheau <mbertheau@gmail.com>
2014-08-04 15:06:05 +0200

    Bump version.
    I half-used 0.0.3 ill-conceivedly in some requirements already, so skip
    that one.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='adyen',
-      version='0.0.2',
+      version='0.0.4',
       description='Python library for the Adyen payment provider',
       license="MIT",
       author='Markus Bertheau',
