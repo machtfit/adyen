eab53be6a35d246467580a587f5aa5436f9a63c2
Markus Bertheau <mbertheau@gmail.com>
2015-02-26 10:21:15 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='adyen',
-      version='0.0.6',
+      version='0.0.7',
       description='Python library for the Adyen payment provider',
       license="MIT",
       author='Markus Bertheau',
