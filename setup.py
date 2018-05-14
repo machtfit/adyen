fe1668051cd1302fe5d0de24f61668d6ad4e51bb
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2018-05-14 15:49:43 +0200

    Update setup.py to reflect python3 change

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='adyen',
       version='0.1.3',
@@ -7,5 +7,16 @@ setup(name='adyen',
       author='Markus Bertheau',
       author_email='mbertheau@gmail.com',
       long_description=open('README.md').read(),
-      packages=['adyen', 'django_adyen', 'django_adyen.templatetags'],
-      install_requires=['pytz', 'zope.dottedname'])
+      packages=find_packages(),
+      install_requires=['pytz', 'zope.dottedname'],
+      classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: Implementation :: CPython'
+            'Environment :: Web Environment',
+            'Framework :: Django'
+      ])
