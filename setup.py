855510be4cb24ceeceea0573ef627fa0d1bba73c
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2018-11-13 16:30:00 +0100

    Remove oscar adyen

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='adyen',
       author='Markus Bertheau',
       author_email='mbertheau@gmail.com',
       long_description=open('README.md').read(),
-      packages=['adyen', 'django_adyen', 'django_adyen.templatetags'],
-      install_requires=['pytz', 'zope.dottedname'])
+      packages=['adyen'],
+      install_requires=['pytz'])
